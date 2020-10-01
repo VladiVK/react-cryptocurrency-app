f1888b5985ca992add0164b7a31d9825fa747c49
VladiVK <kozobon@gmail.com>
2020-10-01 14:18:18 +0300

    deleted some comments

--- src/components/App.js
+++ src/components/App.js
@@ -25,34 +25,4 @@ const App = () => {
 
 export default App;
 
-// https://stackoverflow.com/questions/50155909/how-to-use-context-api-with-react-router-v4/50158702
-
-  /*
-  
-  <CryptosProvider>
-      <SearchBar />
-    <Switch>
-        <Route exact path='/' component={Cryptos } />
-        <Route  path='/:id' component={Crypto } />
-    </Switch>    
-  </CryptosProvider>
-    
-    */
-
-
-/*     
-<ContextA>
-    <Switch>
-        <Route exact path='/' render={ () => <Redirect to='/route1' /> } />
-        <Route path='/route1' component={ Component1 } />
-    </Switch>
-    <ContextB>
-        <Switch>
-            <Route path='/route2' component={ Component2 } />
-            <Route path='/route3' component={ Component3 } />
-        </Switch>        
-    </ContextB>        
-</ContextA>
- */
-
 
