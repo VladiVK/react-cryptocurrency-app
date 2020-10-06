f0e1e4e83686724a92984f2b6cf3ebfe33d73c3b
VladiVK <kozobon@gmail.com>
2020-10-06 14:30:58 +0300

    upload preliminary build version

--- src/contexts/CryptosContext.js
+++ src/contexts/CryptosContext.js
@@ -3,9 +3,9 @@ import cryptoReducer from '../reducers/cryptos.reducer'
 import axios from 'axios';
 
 // const DEMO_KEY = 'demo-26240835858194712a4f8cc0dc635c7a'
-const API_KEY = 'ae04d4b6ed06b87563e71f31fd10bc23'
-const BASE_URL = 'https://api.nomics.com/v1/currencies/ticker?'
-const URL = `${BASE_URL}key=${API_KEY}&interval=1d,7d,30d&convert=USD`
+// const API_KEY = 'ae04d4b6ed06b87563e71f31fd10bc23'
+// const BASE_URL = 'https://api.nomics.com/v1/currencies/ticker?'
+const URL = `https://api.nomics.com/v1/currencies/ticker?key=ae04d4b6ed06b87563e71f31fd10bc23&interval=1d,7d,30d&convert=USD`
 
 const defaultValue = {
     cryptos: [],
@@ -28,8 +28,11 @@ export const CryptosProvider = (props) => {
    
     const [state, dispatch] = useReducer(cryptoReducer, defaultValue)
 
+ 
+
     useEffect( () => {
         axios
+            
             .get(URL)
             .then( response => {
                 const cryptoData = response.data.slice(0,100)
